3fe0f5a02da5fe44ab6f90cdc08481ab0dfc463a
cthermolia-grnet <cthermolia@grnet.gr>
2026-03-11 15:42:12 +0200

    first integration

--- runtime/src/main/java/org/grnet/endpoint/scanner/runtime/entities/ResourceAuthorizationRepository.java
+++ runtime/src/main/java/org/grnet/endpoint/scanner/runtime/entities/ResourceAuthorizationRepository.java
@@ -4,7 +4,11 @@ import java.util.List;
 
 public interface ResourceAuthorizationRepository {
 
-    List<ResourceAuthorization> findAll();
     List<ResourceAuthorization> list(String column, String value);
     void  create(ResourceAuthorization entity);
+    List<ResourceAuthorization> findAllResourceAuthorization();
+    ResourceAuthorization findById(Long id);
+    void update(ResourceAuthorization entity);
+    void delete(Long id);
+
 }
