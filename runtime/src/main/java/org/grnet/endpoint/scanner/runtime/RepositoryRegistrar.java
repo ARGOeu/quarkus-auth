3684214edd2c66d883cd2560f6c99796ede57c3b
cthermolia-grnet <cthermolia@grnet.gr>
2026-03-27 14:19:18 +0200

    annotated repository

--- runtime/src/main/java/org/grnet/endpoint/scanner/runtime/RepositoryRegistrar.java
+++
@@ -1,7 +0,0 @@
-package org.grnet.endpoint.scanner.runtime;
-
-import org.grnet.endpoint.scanner.runtime.resolvers.RepositoryRegistry;
-
-public interface RepositoryRegistrar {
-    void registerRepositories(RepositoryRegistry registry);
-}
