4514c36b262232757185236da335d2fc05039297
cthermolia-grnet <cthermolia@grnet.gr>
2026-06-04 13:13:38 +0300

    minor to check

--- deployment/src/main/java/org/grnet/endpoint/scanner/deployment/EndpointScannerProcessor.java
+++ deployment/src/main/java/org/grnet/endpoint/scanner/deployment/EndpointScannerProcessor.java
@@ -39,7 +39,6 @@ import org.grnet.endpoint.scanner.runtime.repositories.EndpointResolverRepositor
 import org.grnet.endpoint.scanner.runtime.repositories.PersistenceEntitlementRepository;
 import org.grnet.endpoint.scanner.runtime.repositories.ResourceAuthorizationRepository;
 import org.grnet.endpoint.scanner.runtime.repositories.RoleEndpointRepository;
-import org.grnet.endpoint.scanner.runtime.repositories.TestRoleEndpointRepository;
 import org.grnet.endpoint.scanner.runtime.repositories.jdbc.EndpointResolverJdbcRepository;
 import org.grnet.endpoint.scanner.runtime.repositories.jdbc.PersistenceEntitlementJDBCRepository;
 import org.grnet.endpoint.scanner.runtime.repositories.jdbc.ResourceAuthorizationJdbcRepository;
@@ -317,8 +316,7 @@ class EndpointScannerProcessor {
                 AdditionalBeanBuildItem.unremovableOf(RoleEndpointRepository.class),
                 AdditionalBeanBuildItem.unremovableOf(RoleEndpointContext.class),
                 AdditionalBeanBuildItem.unremovableOf(RoleEndpointService.class),
-                AdditionalBeanBuildItem.unremovableOf(RoleEndpointHolder.class),
-                AdditionalBeanBuildItem.unremovableOf(TestRoleEndpointRepository.class));
+                AdditionalBeanBuildItem.unremovableOf(RoleEndpointHolder.class));
 
     }
 
